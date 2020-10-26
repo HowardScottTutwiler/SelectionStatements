88ed1aecf2bfe2b7e8a7fafaa78c213399b30f48
Scott Tutwiler <hstutwiler@gmail.com>
2020-10-26 14:38:22 -0500

    Add project files.

---
+++ SelectionStatements/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace SelectionStatements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int favoriteNumber = 7;
+
+            Console.WriteLine("Guess my favorite number. Hint: less than 10");
+            int userGuess = int.Parse(Console.ReadLine());
+
+            if (userGuess < favoriteNumber)
+            {
+                Console.WriteLine("Too low!");
+            }
+
+            if (userGuess > favoriteNumber)
+            {
+                Console.WriteLine("Too high!");
+            }
+
+            if (userGuess == favoriteNumber)
+            {
+                Console.WriteLine("You are correct!");
+            }
+
+            else
+            {
+                Console.WriteLine("Oh well");
+            }
+        }
+    }
+}
